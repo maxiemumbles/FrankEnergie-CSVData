68bcd79d1ab1c5181d1d071dcb5aa9356c1635b4
Max <max.timmerman@hotmail.com>
2026-03-27 15:40:04 +0100

    UPDATE: Now works for the entire year.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 import java.io.IOException;
 import java.net.http.*;
+import java.time.LocalDate;
 import java.util.ArrayList;
+import java.time.LocalDate;
 
 public class Main {
     static String URL = "https://www.frankenergie.nl/graphql";
@@ -9,19 +11,25 @@ public class Main {
     static void main() {
         CSVWriter csvWriter = new CSVWriter();
         APIHandler apiHandler = new APIHandler();
-        HttpResponse<String> rawResponse = apiHandler.getAPIResponse("2026-03-25"); // DATE FORMATTED IN "YYYY-MM-DD"
-
-        GraphQLResponse response;
-        try {
-            assert rawResponse != null;
-            response = apiHandler.readJsonIntoObject(rawResponse.body());
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
 
         ArrayList<String[]> pricePoints = new ArrayList<>();
-        response.getData().getMarketPrices().getElectricityPrices().forEach(pricePoint ->
-                pricePoints.add(new String[] {pricePoint.getFrom(), String.valueOf(pricePoint.getAllInPrice()), pricePoint.getPerUnit()}));
+
+        LocalDate dateNow = LocalDate.now();
+        for (LocalDate incrementingDate = dateNow.minusYears(1); incrementingDate.isBefore(dateNow); incrementingDate = incrementingDate.plusDays(1)) {
+            System.out.println(incrementingDate);
+            HttpResponse<String> rawResponse = apiHandler.getAPIResponse(incrementingDate.toString()); // DATE FORMATTED IN "YYYY-MM-DD"
+
+            GraphQLResponse response;
+            try {
+                assert rawResponse != null;
+                response = apiHandler.readJsonIntoObject(rawResponse.body());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+
+            response.getData().getMarketPrices().getElectricityPrices().forEach(pricePoint ->
+                    pricePoints.add(new String[] {pricePoint.getFrom(), String.valueOf(pricePoint.getAllInPrice()), pricePoint.getPerUnit()}));
+        }
 
         try {
             csvWriter.writeToCSV(pricePoints);
